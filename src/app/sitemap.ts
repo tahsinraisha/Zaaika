81fd731c154448e384c13d5b11ea182f7492819a
Tahsin Raisha <raivonahstudio@gmail.com>
2026-06-24 02:01:14 +0400

    Delete src directory

--- src/app/sitemap.ts
+++
@@ -1,4 +0,0 @@
-import type { MetadataRoute } from "next";
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [{ url: "https://www.zaaika.co.uk", lastModified: new Date(), changeFrequency: "weekly", priority: 1 }];
-}
